0d8431e30b8ea379a4634abcd96f911592a174a4
vuongtrong2143 <trongtat9@gmail.com>
2026-03-18 14:21:46 +0700

    Them bai 1

---
+++ Bai1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+var cacsochan = from n in numbers
+                where (n % 2 == 0)
+                select n;
+Console.WriteLine("Danh sach cac so chan: ");
+foreach (var n in cacsochan)
+    Console.Write($"{n} ");
