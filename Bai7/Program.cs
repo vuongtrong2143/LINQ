b66e5dab82653826f4e7df80f264e163835d060c
vuongtrong2143 <trongtat9@gmail.com>
2026-03-18 18:44:33 +0700

    Them bai 7

---
+++ Bai7/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+var ds = numbers.Any(x => x > 10);
+Console.WriteLine($"{ds}.");
