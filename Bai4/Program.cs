fceb9d5c19142e024833d0297f2777f3e8f4e651
vuongtrong2143 <trongtat9@gmail.com>
2026-03-18 18:41:59 +0700

    Them bai 4

---
+++ Bai4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<int> numbers = new List<int>() { 24, 150, 53, 12, 21, 43 };
+
+var ds = numbers.OrderByDescending(n => n);
+
+Console.WriteLine("Danh sach cac so theo thu tu giam dan: ");
+foreach (var n in ds)
+    Console.Write($"{n} ");
