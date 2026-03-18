cf449e5a625d74c701f1bf2c617e5c9c9c14904c
vuongtrong2143 <trongtat9@gmail.com>
2026-03-18 18:47:13 +0700

    Them bai 10

---
+++ Bai10/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<Student> students = new List<Student>()
+{
+    new Student { Id = 1, Name = "An", Score = 8 },
+    new Student { Id = 2, Name = "Binh", Score = 6 },
+    new Student { Id = 3, Name = "Chi", Score = 9 },
+    new Student { Id = 4, Name = "Dung", Score = 7 }
+};
+
+var Svgioi = students.OrderByDescending(s => s.Score);
+foreach (var s in Svgioi)
+    Console.WriteLine($"Id: {s.Id}, Name: {s.Name}, Score: {s.Score}.");
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public double Score { get; set; }
+}
