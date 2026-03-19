02afc4fc56c78da67b23d85ec02a82a3dc468225
vuongtrong2143 <trongtat9@gmail.com>
2026-03-19 14:47:56 +0700

    Them bai 16

---
+++ Bai16/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<int> danhsach = new List<int>() { 1, 2, 2, 3, 4, 4, 5 };
+
+var sv = danhsach.Sum();
+Console.Write($"Tong cac phan tu trong danh sach: {sv}.");
