95d604127b9cfa4482266558313e281aff700ff2
vuongtrong2143 <trongtat9@gmail.com>
2026-03-19 14:47:03 +0700

    Them bai 15

---
+++ Bai15/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<int> danhsach = new List<int>() { 1,2,2,3,4,4,5};
+
+var sv = danhsach.Distinct();
+Console.WriteLine("Danh sach sau loai bo phan tu trung: ");
+foreach (int i in sv)
+    Console.Write($"{i} ");
