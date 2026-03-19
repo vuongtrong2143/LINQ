2032d412822d094882ac4d4341014ad8f2d6c924
vuongtrong2143 <trongtat9@gmail.com>
2026-03-19 14:50:53 +0700

    Them bai 19

---
+++ Bai19/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<int> numbers = new List<int> { 1, 3, 4, 2, 3, 3, 1, 5, 3 };
+
+int phantu = numbers.GroupBy(x => x) // Nhom cac so giong nhau lai voi nhau
+                          .OrderByDescending(g => g.Count()) // Sap xep giam dan dua tren so luong phan tu cua moi nhom
+                          .Select(g => g.Key) // Chon ra gia tri dai dien cua moi nhom
+                          .FirstOrDefault(); // Lay phan tu dau tien
+
+Console.WriteLine($"So xuat hien nhieu nhat la: {phantu}");
