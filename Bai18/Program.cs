2f48efd4c56500f14103f7dbbc6a68923ac3466e
vuongtrong2143 <trongtat9@gmail.com>
2026-03-19 14:50:03 +0700

    Them bai 18

---
+++ Bai18/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong - 225LTC#02");
+
+List<Student> students = new List<Student>()
+{
+    new Student { Id = 1, Name = "An", Score = 8 },
+    new Student { Id = 2, Name = "Binh", Score = 6 },
+    new Student { Id = 3, Name = "Chi", Score = 9 },
+    new Student { Id = 4, Name = "Dung", Score = 7 }
+};
+
+var chianhom = students.GroupBy(x =>
+                            x.Score >= 8 ? "Gioi" :
+                            x.Score >= 6 ? "Kha" : "Trung binh");
+foreach (var nhom in chianhom)
+{
+    Console.WriteLine($"Nhom {nhom.Key}");
+    foreach (var s in nhom)
+    {
+        Console.WriteLine($" Id = {s.Id}, Name = {s.Name}, Score = {s.Score}");
+    }
+}
+
+ //   Console.WriteLine($"Sinh vien gioi la: {s.Name}");
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public double Score { get; set; }
+}
